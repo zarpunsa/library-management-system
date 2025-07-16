0cf17f3d55781d566787b2514384d31cb9f074ec
zarpunsa <wiwozzz.bamby@gmail.com>
2025-07-16 20:33:51 +0700

    ok

--- app.py
+++ app.py
@@ -1,102 +1,31 @@
-from flask import Blueprint, g, escape, session, redirect, render_template, request, jsonify, Response, flash
-from app import DAO
+from flask import Flask, g, escape, session, redirect, render_template, request, jsonify, Response
 from Misc.functions import *
 
-from Controllers.UserManager import UserManager
+app = Flask(_name_)
+app.secret_key = '#$ab9&^BB00_.'
 
-user_view = Blueprint('user_routes', __name__, template_folder='/templates')
+# Setting DAO Class
+from Models.DAO import DAO
 
-user_manager = UserManager(DAO)
+DAO = DAO(app)
 
-@user_view.route('/', methods=['GET'])
-def home():
-	g.bg = 1
+# Registering blueprints
+from routes.user import user_view
+from routes.book import book_view
+from routes.admin import admin_view
+from routes.about import about_view
 
-	user_manager.user.set_session(session, g)
-	print(g.user)
+# Registering custom functions to be used within templates
+app.jinja_env.globals.update(
+    ago=ago,
+    str=str,
+)
 
-	return render_template('home.html', g=g)
+app.register_blueprint(user_view)
+app.register_blueprint(book_view)
+app.register_blueprint(admin_view)
+app.register_blueprint(about_view)
 
-
-@user_view.route('/signin', methods=['GET', 'POST'])
-@user_manager.user.redirect_if_login
-def signin():
-	if request.method == 'POST':
-		_form = request.form
-		email = str(_form["email"])
-		password = str(_form["password"])
-
-		if len(email)<1 or len(password)<1:
-			return render_template('signin.html', error="Email and password are required")
-
-		d = user_manager.signin(email, hash(password))
-
-		if d and len(d)>0:
-			session['user'] = int(d['id'])
-
-			return redirect("/")
-
-		return render_template('signin.html', error="Email or password incorrect")
-
-
-	return render_template('signin.html')
-
-
-@user_view.route('/signup', methods=['GET', 'POST'])
-@user_manager.user.redirect_if_login
-def signup():
-	if request.method == 'POST':
-		name = request.form.get('name')
-		email = request.form.get('email')
-		password = request.form.get('password')
-
-		if len(name) < 1 or len(email)<1 or len(password)<1:
-			return render_template('signup.html', error="All fields are required")
-
-		new_user = user_manager.signup(name, email, hash(password))
-
-		if new_user == "already_exists":
-			return render_template('signup.html', error="User already exists with this email")
-
-
-		return render_template('signup.html', msg = "You've been registered!")
-
-
-	return render_template('signup.html')
-
-
-@user_view.route('/signout/', methods=['GET'])
-@user_manager.user.login_required
-def signout():
-	user_manager.signout()
-
-	return redirect("/", code=302)
-
-@user_view.route('/user/', methods=['GET'])
-@user_manager.user.login_required
-def show_user(id=None):
-	user_manager.user.set_session(session, g)
-	
-	if id is None:
-		id = int(user_manager.user.uid())
-
-	d = user_manager.get(id)
-	mybooks = user_manager.getBooksList(id)
-
-	return render_template("profile.html", user=d, books=mybooks, g=g)
-
-@user_view.route('/user', methods=['POST'])
-@user_manager.user.login_required
-def update():
-	user_manager.user.set_session(session, g)
-	
-	_form = request.form
-	name = str(_form["name"])
-	email = str(_form["email"])
-	password = str(_form["password"])
-	bio = str(_form["bio"])
-
-	user_manager.update(name, email, hash(password), bio, user_manager.user.uid())
-
-	flash('Your info has been updated!')
-	return redirect("/user/")
+#@app.route('/about')
+#def about():
+#    return render_template('about.html')
